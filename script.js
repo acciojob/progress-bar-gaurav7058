e8f62d5aace0709df017422d62c5cfca575b0af5
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-08-01 15:55:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your JS code here. If required.
+const circles = document.querySelectorAll('.circle');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+let currentActive = 0;
+
+nextButton.addEventListener('click', () => {
+    currentActive++;
+    update();
+});
+
+prevButton.addEventListener('click', () => {
+    currentActive--;
+    update();
+});
+
+function update() {
+    circles.forEach((circle, idx) => {
+        if (idx <= currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active');
+        }
+    });
+
+    if (currentActive === 0) {
+        prevButton.disabled = true;
+    } else {
+        prevButton.disabled = false;
+    }
+
+    if (currentActive === circles.length - 1) {
+        nextButton.disabled = true;
+    } else {
+        nextButton.disabled = false;
+    }
+}
+
+update();
